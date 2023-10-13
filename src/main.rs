39b55328b282c5acf2b683e889ad4cf4ddcc9fd2
viv-defy <vishnu@MacBook-Pro.local>
2023-10-13 17:54:31 +0530

    made changes to use application port from configuration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Unable to bind to port 8000");
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Unable to bind to port 8000");
     run(listener)?.await
 }
